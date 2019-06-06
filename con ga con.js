637bdcf4b199e4418cf347cd0b214cb859023840
Macbook <macbook@MBP-cua-Macbook.localdomain>
2019-06-06 09:46:10 +0700

    bai tap

---
+++ con ga con.js
@@ -0,0 +1,4 @@
+let inputWidth= parseInt( prompt("Enter the width"));
+let inputHeight = parseInt( prompt("Enter the height"));
+let area = inputHeight*inputWidth;
+alert("the area is:"+area);
--- seikg.js
+++
@@ -1,3 +0,0 @@
-function showmessage() {
-alert('xin chao')
-}
